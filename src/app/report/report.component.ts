88f250bd825ded10c2f70b3c904cb56bc1de6919
teeppope <terraepop@gmail.com>
2016-08-26 11:27:17 -0700

    get posting to work on the report component view

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,6 +1,11 @@
 //This is the report form page component view
 
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { Encounter, IAlien /*Colonist*/ } from '../shared/models';
+// import { ColonistService } from '../shared/services/colonist-service';
+import { EncounterService } from '../shared/services/encounters-service';
+import { AlienService } from '../shared/services/alien-service';
 
 @Component({
   moduleId: module.id,
@@ -8,11 +13,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'report.component.html',
   styleUrls: ['report.component.css']
 })
-export class ReportComponent implements OnInit {
-	title: string = 'Report Encounter!'; 
-  constructor() { }
+export class ReportComponent {
 
-  ngOnInit() {
-  }
+	title: string = 'Report Encounter!';
 
+	NO_ALIEN_SELECTED = '(none)';
+
+	public encounter: Encounter;
+	public aliens: IAlien[];
+
+  constructor(
+	  	private router: Router,
+	  	private encounterService: EncounterService,
+	  	private alienService: AlienService
+  	) { 
+  		this.encounter = new Encounter('', '', '', '555');
+
+  		alienService.getAliens().then(aliens => this.aliens = aliens);
+  	}
+
+  	onSubmit(){
+		this.encounterService.addEncounters({'encounter' : this.encounter}).then(encounter => {
+			this.router.navigate(['../encounters']);
+		});
+	}
+
+  	get noAliens(){
+		return this.encounter.atype === this.NO_ALIEN_SELECTED;
+	}
 }
